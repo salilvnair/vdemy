60dbc4f253bdb3353cd425571dfae653145db292
Salil V Nair <salilvnair@gmail.com>
2019-03-04 15:15:45 +0530

    changed header toolbar with highlight the currently selected

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { AuthService } from '../../auth/service/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +12,28 @@ import { AuthService } from '../../auth/service/auth.service';
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuth: boolean;
   authSubsriciption: Subscription;
+  highLightElement: string;
   @Output() sideNavToggle = new EventEmitter<void>();
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {}
 
   ngOnInit() {
+    this.highlightElementOnRoute();
+    this.initAuthorization();
+  }
+
+  highlightElementOnRoute() {
+    this.router.events.filter((event: any) => 
+                    event instanceof NavigationEnd)
+                    .subscribe(event => {
+                      this.currentlyActive(event.url) 
+                    });
+  }
+  
+  initAuthorization() {
     this.authSubsriciption = this.authService.authChange.subscribe(
       authStatus => {
         this.isAuth = authStatus;
@@ -33,5 +51,17 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onLogout() {
     this.authService.logout();
+  }
+
+  currentlyActive(id) {
+    this.highLightElement = id;
+  }
+  
+  getHighlightColor(id) {
+    if (this.highLightElement && this.highLightElement === id) {
+      return { color: "#3f51b5", "background-color": "white" };
+    } else {
+      return { color: "white", "background-color": "#3f51b5" };
+    }
   }
 }
