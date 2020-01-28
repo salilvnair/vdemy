e86e2a0049d00128b7a4544c5f61de3c0ce11ee5
Salil V Nair <salilvnair@icloud.com>
2020-01-29 00:29:39 +0530

    vdemy online 1.1.3 changed in package and published 3.0.1 ui only build

--- vdemy/gulpfile.js
+++ vdemy/gulpfile.js
@@ -1,9 +1,16 @@
 const { src, dest } = require('gulp');
 var replace = require('gulp-replace')
 
-exports.default = function (cb) {    
-    src(['../vdemy-staging/electron/build/index.html'])
-        .pipe(replace('type="module"', 'type="text/javascript"'))
-        .pipe(dest('../vdemy-staging/electron/build/'));
+exports.default = function (cb) {
+    //// Uncomment below while Electron build
+    // src(['../vdemy-staging/electron/build/index.html'])
+    //     .pipe(replace('type="module"', 'type="text/javascript"'))
+    //     .pipe(dest('../vdemy-staging/electron/build/'));
+    // cb();
+
+    //// Uncomment below while Angular build
+    src(['../vdemy-staging/angular/build/resources/app/build/index.html'])
+    .pipe(replace('type="module"', 'type="text/javascript"'))
+    .pipe(dest('../vdemy-staging/angular/build/resources/app/build'));
     cb();
 };
